66d889bdd42260d95ebdc062b2ff23922980f7b7
Daniel Nilsson <daniel.nilsson@appgate.com>
2021-05-03 16:37:12 +0200

    PATCH saml identity provider
    based on discriminator of IP

--- api/v15/openapi/client.go
+++ api/v15/openapi/client.go
@@ -126,6 +126,8 @@ type APIClient struct {
 	LdapIdentityProvidersApi *LdapIdentityProvidersApiService
 
 	RadiusIdentityProvidersApi *RadiusIdentityProvidersApiService
+
+	SamlIdentityProvidersApi *SamlIdentityProvidersApiService
 }
 
 type service struct {
@@ -185,6 +187,7 @@ func NewAPIClient(cfg *Configuration) *APIClient {
 	// since openapi.generator does not play well with discriminator from the open api spec.
 	c.LdapIdentityProvidersApi = (*LdapIdentityProvidersApiService)(&c.common)
 	c.RadiusIdentityProvidersApi = (*RadiusIdentityProvidersApiService)(&c.common)
+	c.SamlIdentityProvidersApi = (*SamlIdentityProvidersApiService)(&c.common)
 
 	return c
 }
