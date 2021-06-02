12743ed0a6aea6d17b94d735fba5344be215b074
Daniel Nilsson <daniel.nilsson@appgate.com>
2021-06-02 14:36:09 +0200

    add deprecated attributes back to site.

--- api/v15/openapi/model_site_all_of_vpn.go
+++ api/v15/openapi/model_site_all_of_vpn.go
@@ -26,6 +26,14 @@ type SiteAllOfVpn struct {
 	RouteVia *SiteAllOfVpnRouteVia `json:"routeVia,omitempty"`
 	// Whether to enable URL Access feature or not.
 	UrlAccessEnabled *bool `json:"urlAccessEnabled,omitempty"`
+	// Flag for manipulating web proxy p12 file. Setting this false will delete the existing p12 file from database.
+	WebProxyEnabled *bool `json:"webProxyEnabled,omitempty"`
+	// The PKCS12 package to be used for web proxy. The file must be with no password and must include the full certificate chain and a private key. In Base64 format.
+	WebProxyKeyStore *string `json:"webProxyKeyStore,omitempty"`
+	// Gateway will verify the certificate of the endpoints.
+	WebProxyVerifyUpstreamCertificate *bool `json:"webProxyVerifyUpstreamCertificate,omitempty"`
+	// The subject name of the certificate with private key in the PKCS12 file for web proxy assigned to this site.
+	WebProxyCertificateSubjectName *string `json:"webProxyCertificateSubjectName,omitempty"`
 	// P12 files for proxying traffic for URL Access feature.
 	UrlAccessP12s *[]P12 `json:"urlAccessP12s,omitempty"`
 	// Frequency configuration for generating IP Access audit logs for a connection.
@@ -42,6 +50,8 @@ func NewSiteAllOfVpn() *SiteAllOfVpn {
 	this.StateSharing = &stateSharing
 	var snat bool = false
 	this.Snat = &snat
+	var webProxyVerifyUpstreamCertificate bool = true
+	this.WebProxyVerifyUpstreamCertificate = &webProxyVerifyUpstreamCertificate
 	var ipAccessLogIntervalSeconds float32 = 120
 	this.IpAccessLogIntervalSeconds = &ipAccessLogIntervalSeconds
 	return &this
@@ -56,6 +66,8 @@ func NewSiteAllOfVpnWithDefaults() *SiteAllOfVpn {
 	this.StateSharing = &stateSharing
 	var snat bool = false
 	this.Snat = &snat
+	var webProxyVerifyUpstreamCertificate bool = true
+	this.WebProxyVerifyUpstreamCertificate = &webProxyVerifyUpstreamCertificate
 	var ipAccessLogIntervalSeconds float32 = 120
 	this.IpAccessLogIntervalSeconds = &ipAccessLogIntervalSeconds
 	return &this
@@ -253,6 +265,134 @@ func (o *SiteAllOfVpn) SetUrlAccessEnabled(v bool) {
 	o.UrlAccessEnabled = &v
 }
 
+// GetWebProxyEnabled returns the WebProxyEnabled field value if set, zero value otherwise.
+func (o *SiteAllOfVpn) GetWebProxyEnabled() bool {
+	if o == nil || o.WebProxyEnabled == nil {
+		var ret bool
+		return ret
+	}
+	return *o.WebProxyEnabled
+}
+
+// GetWebProxyEnabledOk returns a tuple with the WebProxyEnabled field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *SiteAllOfVpn) GetWebProxyEnabledOk() (*bool, bool) {
+	if o == nil || o.WebProxyEnabled == nil {
+		return nil, false
+	}
+	return o.WebProxyEnabled, true
+}
+
+// HasWebProxyEnabled returns a boolean if a field has been set.
+func (o *SiteAllOfVpn) HasWebProxyEnabled() bool {
+	if o != nil && o.WebProxyEnabled != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetWebProxyEnabled gets a reference to the given bool and assigns it to the WebProxyEnabled field.
+func (o *SiteAllOfVpn) SetWebProxyEnabled(v bool) {
+	o.WebProxyEnabled = &v
+}
+
+// GetWebProxyKeyStore returns the WebProxyKeyStore field value if set, zero value otherwise.
+func (o *SiteAllOfVpn) GetWebProxyKeyStore() string {
+	if o == nil || o.WebProxyKeyStore == nil {
+		var ret string
+		return ret
+	}
+	return *o.WebProxyKeyStore
+}
+
+// GetWebProxyKeyStoreOk returns a tuple with the WebProxyKeyStore field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *SiteAllOfVpn) GetWebProxyKeyStoreOk() (*string, bool) {
+	if o == nil || o.WebProxyKeyStore == nil {
+		return nil, false
+	}
+	return o.WebProxyKeyStore, true
+}
+
+// HasWebProxyKeyStore returns a boolean if a field has been set.
+func (o *SiteAllOfVpn) HasWebProxyKeyStore() bool {
+	if o != nil && o.WebProxyKeyStore != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetWebProxyKeyStore gets a reference to the given string and assigns it to the WebProxyKeyStore field.
+func (o *SiteAllOfVpn) SetWebProxyKeyStore(v string) {
+	o.WebProxyKeyStore = &v
+}
+
+// GetWebProxyVerifyUpstreamCertificate returns the WebProxyVerifyUpstreamCertificate field value if set, zero value otherwise.
+func (o *SiteAllOfVpn) GetWebProxyVerifyUpstreamCertificate() bool {
+	if o == nil || o.WebProxyVerifyUpstreamCertificate == nil {
+		var ret bool
+		return ret
+	}
+	return *o.WebProxyVerifyUpstreamCertificate
+}
+
+// GetWebProxyVerifyUpstreamCertificateOk returns a tuple with the WebProxyVerifyUpstreamCertificate field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *SiteAllOfVpn) GetWebProxyVerifyUpstreamCertificateOk() (*bool, bool) {
+	if o == nil || o.WebProxyVerifyUpstreamCertificate == nil {
+		return nil, false
+	}
+	return o.WebProxyVerifyUpstreamCertificate, true
+}
+
+// HasWebProxyVerifyUpstreamCertificate returns a boolean if a field has been set.
+func (o *SiteAllOfVpn) HasWebProxyVerifyUpstreamCertificate() bool {
+	if o != nil && o.WebProxyVerifyUpstreamCertificate != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetWebProxyVerifyUpstreamCertificate gets a reference to the given bool and assigns it to the WebProxyVerifyUpstreamCertificate field.
+func (o *SiteAllOfVpn) SetWebProxyVerifyUpstreamCertificate(v bool) {
+	o.WebProxyVerifyUpstreamCertificate = &v
+}
+
+// GetWebProxyCertificateSubjectName returns the WebProxyCertificateSubjectName field value if set, zero value otherwise.
+func (o *SiteAllOfVpn) GetWebProxyCertificateSubjectName() string {
+	if o == nil || o.WebProxyCertificateSubjectName == nil {
+		var ret string
+		return ret
+	}
+	return *o.WebProxyCertificateSubjectName
+}
+
+// GetWebProxyCertificateSubjectNameOk returns a tuple with the WebProxyCertificateSubjectName field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *SiteAllOfVpn) GetWebProxyCertificateSubjectNameOk() (*string, bool) {
+	if o == nil || o.WebProxyCertificateSubjectName == nil {
+		return nil, false
+	}
+	return o.WebProxyCertificateSubjectName, true
+}
+
+// HasWebProxyCertificateSubjectName returns a boolean if a field has been set.
+func (o *SiteAllOfVpn) HasWebProxyCertificateSubjectName() bool {
+	if o != nil && o.WebProxyCertificateSubjectName != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetWebProxyCertificateSubjectName gets a reference to the given string and assigns it to the WebProxyCertificateSubjectName field.
+func (o *SiteAllOfVpn) SetWebProxyCertificateSubjectName(v string) {
+	o.WebProxyCertificateSubjectName = &v
+}
+
 // GetUrlAccessP12s returns the UrlAccessP12s field value if set, zero value otherwise.
 func (o *SiteAllOfVpn) GetUrlAccessP12s() []P12 {
 	if o == nil || o.UrlAccessP12s == nil {
@@ -336,6 +476,18 @@ func (o SiteAllOfVpn) MarshalJSON() ([]byte, error) {
 	}
 	if o.UrlAccessEnabled != nil {
 		toSerialize["urlAccessEnabled"] = o.UrlAccessEnabled
+	}
+	if o.WebProxyEnabled != nil {
+		toSerialize["webProxyEnabled"] = o.WebProxyEnabled
+	}
+	if o.WebProxyKeyStore != nil {
+		toSerialize["webProxyKeyStore"] = o.WebProxyKeyStore
+	}
+	if o.WebProxyVerifyUpstreamCertificate != nil {
+		toSerialize["webProxyVerifyUpstreamCertificate"] = o.WebProxyVerifyUpstreamCertificate
+	}
+	if o.WebProxyCertificateSubjectName != nil {
+		toSerialize["webProxyCertificateSubjectName"] = o.WebProxyCertificateSubjectName
 	}
 	if o.UrlAccessP12s != nil {
 		toSerialize["urlAccessP12s"] = o.UrlAccessP12s
