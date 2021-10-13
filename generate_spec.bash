9e10b4bd4ee8353cbf6887a19408deaf00cf6bda
Daniel Nilsson <daniel.nilsson@appgate.com>
2021-10-13 15:06:08 +0200

    check for apigentools before executing the script

--- generate_spec.bash
+++ generate_spec.bash
@@ -2,8 +2,23 @@
 
 set -eou pipefail
 
+if ! command -v apigentools &> /dev/null; then
+    echo "apigentools could not be found"
+    echo "Get it - https://github.com/DataDog/apigentools#get-it"
+    exit 1
+fi
 
+
+# Starting from v16 we will use apigentools to generate the sdk with openapi-generator,
+# older versions are not supported by apigentools.
 supportedVersions=(16)
+
+
+
+for version in "${supportedVersions[@]}"; do
+    # remove any existing spec files and download a fresh copy again
+    rm -rf "spec/spec/v$version"
+done
 
 for version in "${supportedVersions[@]}"; do
     git clone git@github.com:appgate/sdp-api-specification.git --single-branch --branch "version-$version" "spec/spec/v$version"
