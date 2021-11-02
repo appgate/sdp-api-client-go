a663983442984af0d368914ab4f3f0b8c9eb9806
Daniel Nilsson <daniel.nilsson@appgate.com>
2021-11-02 13:53:28 +0100

    generated stub login response HTTP 406

--- api/v16/openapi/api_login.go
+++ api/v16/openapi/api_login.go
@@ -892,6 +892,16 @@ func (a *LoginApiService) LoginPostExecute(r ApiLoginPostRequest) (LoginResponse
 			newErr.model = v
 			return localVarReturnValue, localVarHTTPResponse, newErr
 		}
+		if localVarHTTPResponse.StatusCode == 406 {
+			var v InlineResponse406
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
 		if localVarHTTPResponse.StatusCode == 422 {
 			var v ValidationError
 			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
