f141465c4b23d724b092e04471d54ea0de0c95d8
Daniel Nilsson <daniel.nilsson@appgate.com>
2022-11-29 10:52:28 +0100

    append v18

--- generate_spec.bash
+++ generate_spec.bash
@@ -11,7 +11,7 @@ fi
 
 # Starting from v16 we will use apigentools to generate the sdk with openapi-generator,
 # older versions are not supported by apigentools.
-supportedVersions=(16 17)
+supportedVersions=(16 17 18)
 
 
 
