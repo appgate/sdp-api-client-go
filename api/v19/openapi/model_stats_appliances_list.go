fe2d19164d9ea3a1d9d3902654e3d611a5897c83
Masaya Ando <masaya.ando@appgate.com>
2023-06-22 12:52:20 -0400

    Ignore 'query' in StatsApplianceList to unblock 6.0 upgrade

--- api/v19/openapi/model_stats_appliances_list.go
+++ api/v19/openapi/model_stats_appliances_list.go
@@ -32,9 +32,6 @@ type StatsAppliancesList struct {
 	Descending *bool `json:"descending,omitempty"`
 	// The queries applied to the list.
 	Queries []string `json:"queries,omitempty"`
-	// The first query applied to the list. Deprecated as of 6.0. Use queries instead.
-	// Deprecated
-	Query *string `json:"query,omitempty"`
 	// The filters applied to the list.
 	FilterBy []FilterBy `json:"filterBy,omitempty"`
 	// The number of active Appliances with the Controller role enabled.
@@ -293,41 +290,6 @@ func (o *StatsAppliancesList) HasQueries() bool {
 // SetQueries gets a reference to the given []string and assigns it to the Queries field.
 func (o *StatsAppliancesList) SetQueries(v []string) {
 	o.Queries = v
-}
-
-// GetQuery returns the Query field value if set, zero value otherwise.
-// Deprecated
-func (o *StatsAppliancesList) GetQuery() string {
-	if o == nil || o.Query == nil {
-		var ret string
-		return ret
-	}
-	return *o.Query
-}
-
-// GetQueryOk returns a tuple with the Query field value if set, nil otherwise
-// and a boolean to check if the value has been set.
-// Deprecated
-func (o *StatsAppliancesList) GetQueryOk() (*string, bool) {
-	if o == nil || o.Query == nil {
-		return nil, false
-	}
-	return o.Query, true
-}
-
-// HasQuery returns a boolean if a field has been set.
-func (o *StatsAppliancesList) HasQuery() bool {
-	if o != nil && o.Query != nil {
-		return true
-	}
-
-	return false
-}
-
-// SetQuery gets a reference to the given string and assigns it to the Query field.
-// Deprecated
-func (o *StatsAppliancesList) SetQuery(v string) {
-	o.Query = &v
 }
 
 // GetFilterBy returns the FilterBy field value if set, zero value otherwise.
@@ -640,9 +602,6 @@ func (o StatsAppliancesList) MarshalJSON() ([]byte, error) {
 	}
 	if o.Queries != nil {
 		toSerialize["queries"] = o.Queries
-	}
-	if o.Query != nil {
-		toSerialize["query"] = o.Query
 	}
 	if o.FilterBy != nil {
 		toSerialize["filterBy"] = o.FilterBy
